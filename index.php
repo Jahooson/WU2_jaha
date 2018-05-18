15dcb1fcc816acdc207aeb78de2d3a031932fac5
Jahooson <32328540+Jahooson@users.noreply.github.com>
2018-05-18 13:20:57 +0200

    mall och css
    woooohh

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+/*
+require "resources/includes/head.php"; //Kommentar här
+require "resources/views/header.php"; //Kommentar här
+
+1. Skapa en lista över vad menyn/nav skall bestå utav.
+2. Vad skall var och en av sidorna innehålla?
+3. Skall det finnas några krav för att besöka en av "undersidorna"?
+
+- [Home] - Startsida
+    -Möjlighet att logga in.
+    -Möjlighet att singe in
+    -Möjlighet att välja användare
+    -Möjlighet att gå till porifil
+    -Möjlihet att gå till vänner
+    -Möjlihet att gå till posts
+    -Möjlighet att gå till dicotioner
+
+        -[profil]
+            -Se dina egna posts
+            -Se dina egna discotioner
+            -Se poersonlig info
+
+        -[vänner]
+            -Se dina vänner
+            -Möjlighet att skriva medelanden med vänner
+            -Se vännförfrågningar
+            -Se vänn förslag
+
+        -[postes]
+            -Se nya posts
+            -Se alla posts
+
+        -[discotioner]
+            -Se nya discotioner
+            -Se alla discotioner
+            -Möjlighet att gå med discotioner
+- SHOW NEW POSTS
+- SHOW ONLINE FRIENDS
+- SHOW COMMENTS AND LIKES
+
+
+require "resources/views/footer.php"; //Kommentar här
+*/
+  ?>
