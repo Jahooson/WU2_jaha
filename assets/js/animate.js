799f7615e43b0c594d0920a1602b67ac82ae3244
Jahooson <32328540+Jahooson@users.noreply.github.com>
2018-05-31 13:49:50 +0200

    nu du

---
+++ assets/js/animate.js
@@ -0,0 +1,15 @@
+// Wrap every letter in a span
+$('.ml16').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml16 .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: function(el, i) {
+      return 30 * i;
+    }
+});
